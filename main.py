62fa3c06c8b0d8f83191a8fc089787775d8a581a
exprees-store <info.express.store5@gmail.com>
2026-09-11 11:27:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -194,7 +194,6 @@ def convert_blog_to_script(payload: ArticleRequest):
             "generated_video_script": video_script,
             "message": "Blog processed successfully!"
         }
-        .............................................................................
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
